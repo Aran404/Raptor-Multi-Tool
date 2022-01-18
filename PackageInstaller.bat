09cbda0b02908e5ad3f8604a23ef0ab9fa14b340
Aran <96021763+Aran404@users.noreply.github.com>
2022-01-18 18:14:18 -0500

    Create PackageInstaller.bat

---
+++ PackageInstaller.bat
@@ -0,0 +1,15 @@
+@echo off
+pip install colorama
+pip install selenium
+pip install discord
+pip install discum
+pip install requests
+pip install websocket
+pip install pyautogui
+pip install webbrowser
+pip install pyinstaller
+pip install discum
+pip install ctypes
+cls
+echo Packages have been installed
+pause
